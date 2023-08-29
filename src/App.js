76af375cfab7b5e268418bf6f327ab01e5076649
unknown <ruanpletsch@gmail.com>
2023-08-29 10:27:59 -0300

    Sistema de consumo da API viacep

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [endereco, setEndereco] = useState({})
+
+  function manipularEndereco (evento) {
+    
+    let cep = evento.target.value
+    cep = cep.replaceAll(/\D+/g, '')
+
+    setEndereco ({
+      cep
+    })
+
+    if(cep && cep.length === 8) {
+      //obter o cep
+      fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(resposta => resposta.json())
+        .then(dados => {
+          setEndereco(enderecoAntigo => {
+            return {
+              ...enderecoAntigo,
+              bairro: dados.bairro,
+              cidade: dados.localidade,
+              estado: dados.uf
+            }
+          })
+        })
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input placeholder='Digite o CEP' onChange={manipularEndereco}/>
+        <ul>CEP: {endereco.cep}</ul>
+        <ul>Bairro: {endereco.bairro}</ul>
+        <ul>Cidade: {endereco.cidade}</ul>
+        <ul>Estado: {endereco.estado}</ul>
       </header>
     </div>
   );
